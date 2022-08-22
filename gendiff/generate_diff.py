64e9095c44a0f92f99211aa34f91f752957cec76
di8ry <d.nosov1005@mail.ru>
2022-08-22 19:12:23 +0700

    22.08

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -16,7 +16,7 @@ def get_data(file_path):
             return yaml.load(fy, Loader=yaml.FullLoader)
 
 
-def difference(date_1, date_2, format='json', parent=None):
+def generate_diff(date_1, date_2, format='stylish', parent=None):
     result = {}
     result_plain = []
     for key in date_1:
@@ -25,18 +25,16 @@ def difference(date_1, date_2, format='json', parent=None):
                 result[f'{key} 0'] = date_1[key]
             else:
                 if type(date_1[key]) == dict and type(date_2[key]) == dict:
-                    if format == 'json':
-                        result[f'{key} 0'] = difference(date_1[key], date_2[key], format='json')
+                    if format == 'stylish':
+                        result[f'{key} 0'] = generate_diff(date_1[key], date_2[key], format='stylish')
                     elif format == 'plain':
-                        # value_1 = to_plain(date_1[key])
-                        # value_2 = to_plain(date_2[key])
                         value_1 = date_1[key]
                         value_2 = date_2[key]
                         if parent:
                             key = parent + '.' + key
-                        result_plain += difference(value_1, value_2, format='plain', parent=key)
+                        result_plain += generate_diff(value_1, value_2, format='plain', parent=key)
                 else:
-                    if format == 'json':
+                    if format == 'stylish':
                         result[f'{key} 1'] = date_1[key]
                         result[f'{key} 2'] = date_2[key]
                     elif format == 'plain':
@@ -46,7 +44,7 @@ def difference(date_1, date_2, format='json', parent=None):
                             key = parent + '.' + key
                         result_plain.append(f"Property '{key}' was updated. From {value_1} to {value_2}")
         else:
-            if format == 'json':
+            if format == 'stylish':
                 result[f'{key} 1'] = date_1[key]
             elif format == 'plain':
                 if parent:
@@ -54,7 +52,7 @@ def difference(date_1, date_2, format='json', parent=None):
                 result_plain.append(f"Property '{key}' was removed")
     for key in date_2:
         if key not in date_1:
-            if format == 'json':
+            if format == 'stylish':
                 result[f'{key} 2'] = date_2[key]
             elif format == 'plain':
                 value = to_plain(date_2[key])
@@ -65,7 +63,6 @@ def difference(date_1, date_2, format='json', parent=None):
         return result_plain
     return result
 
-#         Property 'common.setting3' was updated. From true to null
 
 def to_plain(value):
     if type(value) == str:
@@ -80,38 +77,11 @@ def to_plain(value):
         return '[complex value]'
 
 
-
-def get_final(result, level=1):
-    result_1 = sorted(result.items())
-    final_str = '{\n'
-    for key, value in result_1:
-        key = replace_digit(key)
-        if type(value) == dict:
-            value = get_final(value, level + 1)
-        for_replace = {'True': 'true', 'False': 'false', 'None': 'null'}
-        for k, v in for_replace.items():
-            value = str(value).replace(k, v)
-        row = f'{"    " * level}{key}: {value}\n'
-        final_str += row
-    final_str += f'{"    " * (level - 1)}' + '}'
-    return final_str
-
-
-def replace_digit(key):
-    if '1' in key[-1:]:
-        return f'- {key.split()[0]}'
-    elif '0' in key[-1:]:
-        return f'0  {key.split()[0]}'
-    elif '2' in key[-1:]:
-        return f'+ {key.split()[0]}'
-    return key
-
-
-if __name__ == '__main__':
-    date_1 = get_data("simple_before.json")
-    date_2 = get_data("simple_after.json")
-    result = (difference(date_1, date_2))
-    print(result)
-    with open('result2.txt', 'w') as f:
-        final_str = get_final(result).replace('  +', '+').replace('  -', '-').replace('  0', '')
-        f.write(final_str)
+# if __name__ == '__main__':
+#     date_1 = get_data("simple_before.json")
+#     date_2 = get_data("simple_after.json")
+#     result = (difference(date_1, date_2))
+#     print(result)
+    # with open('result2.txt', 'w') as f:
+    #     final_str = get_final(result).replace('  +', '+').replace('  -', '-').replace('  0', '')
+    #     f.write(final_str)
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,29 +1,16 @@
-import argparse
-from gendiff.generate_diff import difference, get_final, get_data
+from gendiff.cli import parser
+from gendiff.generate_diff import generate_diff, get_data
+from gendiff.formatters import formatter
 
+def main():
+    args = parser()
+    date_1 = args.first_file
+    date_2 = args.second_file
+    format_ = args.format
 
-def main(date_1=None, date_2=None, format_=None):
-    if not date_1:
-        parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
-        parser.add_argument('first_file')
-        parser.add_argument('second_file')
-        parser.add_argument(
-            '-f',
-            '--format',
-            help='set format of output',
-            default='json'
-        )
-        args = parser.parse_args()
-        date_1 = args.first_file
-        date_2 = args.second_file
-        format_ = args.format
-
-    result = difference(get_data(date_1), get_data(date_2), format=format_)
-    if format_ == 'plain':
-        return '\n'.join(sorted(result))
-    else:
-        return get_final(result).replace('  +', '+').replace('  -', '-').replace('  0', '')
+    result = generate_diff(get_data(date_1), get_data(date_2), format=format_)
+    return formatter(result, format_)
 
 
 if __name__ == '__main__':
-    main()
+    print(main())
