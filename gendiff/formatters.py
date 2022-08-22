83386e4368d05308c2d9484a64f9af1ef8f0b473
di8ry <d.nosov1005@mail.ru>
2022-08-22 19:13:35 +0700

    22.08_1

---
+++ gendiff/formatters.py
@@ -0,0 +1,34 @@
+def formatter(result, format_):
+    if not format_:
+        format_ = 'stylish'
+    if format_ == 'plain':
+        return '\n'.join(sorted(result))
+    elif format_ == 'stylish':
+        final = get_tree(result).replace('  +', '+').replace('  -', '-').replace('  0', '')
+        return final
+
+
+def get_tree(result, level=1):
+    result_1 = sorted(result.items())
+    final_str = '{\n'
+    for key, value in result_1:
+        key = replace_digit(key)
+        if type(value) == dict:
+            value = get_tree(value, level + 1)
+        for_replace = {'True': 'true', 'False': 'false', 'None': 'null'}
+        for k, v in for_replace.items():
+            value = str(value).replace(k, v)
+        row = f'{"    " * level}{key}: {value}\n'
+        final_str += row
+    final_str += f'{"    " * (level - 1)}' + '}'
+    return final_str
+
+
+def replace_digit(key):
+    if '1' in key[-1:]:
+        return f'- {key.split()[0]}'
+    elif '0' in key[-1:]:
+        return f'0  {key.split()[0]}'
+    elif '2' in key[-1:]:
+        return f'+ {key.split()[0]}'
+    return key
